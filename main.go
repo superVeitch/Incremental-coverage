9f0505f5ad870b6ab129ad6ee55624e627d8a295
veitch <wangweiqi@bilibili.com>
2019-10-28 21:37:59 +0800

    fix

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/tools/cover"
 	"io/ioutil"
+	"strings"
 )
 
 func setup() *Diff {
-	byt, err := ioutil.ReadFile("example.diff")
+	byt, err := ioutil.ReadFile("example1.diff")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -14,7 +16,7 @@ func setup() *Diff {
 	return diff
 }
 
-type DiffLine1 struct {
+type DiffFileRe struct {
 	Name string
 	Lines []int
 }
@@ -25,19 +27,53 @@ func main()  {
 	files := setup()
 
 
-	//profile := cover.ParseProfiles("coverage.cov")
-	var diffs []DiffLine
+	profile, err := cover.ParseProfiles("coverage.cov")
+	if err != nil {
+		fmt.Println("解析cover文件异常")
+	}
+
+
+	var diffs []DiffFileRe
 	for _, f := range files.Files {
+		var dl = make([]*DiffLine, 0)
 		for _, h := range f.Hunks  {
-			i := h.NewRange.Lines
-			filter(i.)
+			i := filter(h.NewRange.Lines)
+			dl = append(dl, i...)
+		}
+
+		lines := coverFilter(f.NewName, dl, profile)
+		if len(lines) > 0 {
+			re := DiffFileRe{
+				Name:  f.NewName,
+				Lines: lines,
+			}
+			diffs = append(diffs, re)
 		}
 	}
 
+
+	fmt.Println(diffs)
 }
 
-func filter(dl []DiffLine) []DiffLine{
-	r := make([]DiffLine, 0)
+func coverFilter(fileName string, dl []*DiffLine, f []*cover.Profile) []int {
+	dd := make([]int, 0)
+	for _, d := range dl {
+		for _, f1 := range f {
+			name := strings.TrimPrefix(f1.FileName, "bible-go/")
+			if name == fileName{
+				for _, b := range f1.Blocks  {
+					if b.EndLine >= d.Number && b.StartLine <= d.Number {
+						dd = append(dd, d.Number)
+					}
+				}
+			}
+		}
+	}
+	return dd
+}
+
+func filter(dl []*DiffLine) []*DiffLine {
+	r := make([]*DiffLine, 0)
 	for _, v := range dl {
 		if v.Mode == ADDED {
 			r = append(r, v)
